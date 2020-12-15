29d4841f89febd26897ab61827a07eb4a5a9610e
Lorena Zuazo <pepa_zz@hotmail.com>
2020-12-15 09:56:39 -0300

    tarea seccion 9

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import HooksApp from './HooksApp';
+//import CounterApp from './components/01-useState/CounterApp';
+//import { CounterWhitCustomHooks } from './components/01-useState/CounterWhitCustomHooks';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { FormWithCustomHooks } from './components/02-useEffect/FormWithCustomHooks';
+//import { MultipleCustomHooks } from './components/03-example/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import RealExampleRef from './components/04-useRef/RealExampleRef';
+//import { Layout } from './components/05-useLayoutEffect/Layout';
+//import Memorize from './components/06-memos/Memorize';
+//import MemoHook from './components/06-memos/MemoHook';
+//import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
+
+import './index.css';
+
+ReactDOM.render(
+  <Padre />,
+  document.getElementById('root')
+);
+
